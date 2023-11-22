1f0387589cf24a3eb37ed1a9b5dc6b623e1dac05
Sammsin <144893247+Sammsin@users.noreply.github.com>
2023-11-22 15:17:32 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import pytest
 
-with assertRaises(FileNotFoundError):
-    print("FileNotFoundError detected")
+def test_file_not_found_error():
+    with pytest.raises(FileNotFoundError):
+        # Code that should raise FileNotFoundError
+        print("FileNotFoundError detected")
