a3c7bbd476c9d710c00648702d9c179f4c47097e
Sammsin <144893247+Sammsin@users.noreply.github.com>
2023-11-22 15:18:59 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import pytest
 
+def code_that_raises_file_not_found_error():
+    # Some code that should raise FileNotFoundError
+    raise FileNotFoundError("File not found")
+
 def test_file_not_found_error():
     with pytest.raises(FileNotFoundError):
-        # Code that should raise FileNotFoundError
-        print("FileNotFoundError detected")
+        code_that_raises_file_not_found_error()
