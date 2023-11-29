f05dfc2348cf2768220caea660e8780ec354c883
basicJavaCoder <43179251+basicJavaCoder@users.noreply.github.com>
2023-11-29 12:46:34 +0000

    added unittest to test.py

--- test.py
+++ test.py
@@ -1,9 +1,31 @@
+import unittest
 import pytest
+import race_results
+
 
 def code_that_raises_file_not_found_error():
     # Some code that should raise FileNotFoundError
     raise FileNotFoundError("File not found")
 
+
 def test_file_not_found_error():
     with pytest.raises(FileNotFoundError):
         code_that_raises_file_not_found_error()
+
+
+class TestRaceResults(unittest.TestCase):
+    def test_race_results(self):
+        # Set up some test data
+        races_location = ["Race 1", "Race 2", "Race 3"]
+
+        # Call the function with the test data
+        id, time_taken, venue = race_results.race_results(races_location)
+
+        # Check that the function returned the correct values
+        self.assertEqual(id, "Expected id")
+        self.assertEqual(time_taken, "Expected time_taken")
+        self.assertEqual(venue, "Expected venue")
+
+
+if __name__ == '__main__':
+    unittest.main()
