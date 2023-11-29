5546fdee112eb90f02ccb9fa0f557594986e3b67
Sammsin <144893247+Sammsin@users.noreply.github.com>
2023-11-29 12:57:10 +0000

    Update test.py

--- test.py
+++ test.py
@@ -13,19 +13,6 @@ def test_file_not_found_error():
         code_that_raises_file_not_found_error()
 
 
-class TestRaceResults(unittest.TestCase):
-    def test_race_results(self):
-        # Set up some test data
-        races_location = ["Race 1", "Race 2", "Race 3"]
-
-        # Call the function with the test data
-        id, time_taken, venue = race_results.race_results(races_location)
-
-        # Check that the function returned the correct values
-        self.assertEqual(id, "Expected id")
-        self.assertEqual(time_taken, "Expected time_taken")
-        self.assertEqual(venue, "Expected venue")
-
 
 if __name__ == '__main__':
     unittest.main()
