b274e33872325086c550fa6dee7c95636b788af1
basicJavaCoder <43179251+basicJavaCoder@users.noreply.github.com>
2023-11-29 14:47:26 +0000

    Fixed temporrary test

--- Code_Base.py
+++ Code_Base.py
@@ -33,7 +33,7 @@ def read_integer(prompt):
 
 
 def runners_data():
-    with open("runners1.txt") as input:
+    with open("runners.txt") as input:
         lines = input.readlines()
     runners_name = []
     runners_id = []
