49880704f8ad67d36e2a19f2ab399436a30709fe
basicJavaCoder <43179251+basicJavaCoder@users.noreply.github.com>
2023-11-29 14:42:39 +0000

    Running a test

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import pytest
 
+from Code_Base import runners_data
+
 
 def code_that_raises_file_not_found_error():
     # Some code that should raise FileNotFoundError
+    runners_data()
     raise FileNotFoundError("File not found")
 
 
